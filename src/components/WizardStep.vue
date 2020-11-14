3bff5008d826867a380e1ba87a4968308be05b23
Niels Ganser <niels@ganser.consulting>
2020-11-14 16:25:48 +0100

    Remove <transition>s from WizardStep.vue

--- src/components/WizardStep.vue
+++ src/components/WizardStep.vue
@@ -11,23 +11,20 @@
            :class="{checked: tab.checked,square_shape:isStepSquare, tab_shape:isTabShape}"
            :style="[tab.checked ? stepCheckedStyle : {}, tab.validationError ? errorStyle : {}]">
 
-        <transition :name="transition" mode="out-in">
-
-            <div v-if="tab.active" class="wizard-icon-container"
-                 :class="{square_shape:isStepSquare, tab_shape:isTabShape}"
-                 :style="[tab.active ? iconActiveStyle: {}, tab.validationError ? errorStyle : {}]">
-              <slot name="active-step">
-                <img v-if="tab.image" :src="tab.image">
-                <i v-else-if="tab.icon" :class="tab.icon" class="wizard-icon"></i>
-                <i v-else class="wizard-icon">{{index + 1}}</i>
-              </slot>
-            </div>
-            <slot v-if="!tab.active">
-              <img v-if="tab.image" :src="tab.image">
-              <i v-else-if="!tab.active && tab.icon" :class="tab.icon" class="wizard-icon"></i>
-              <i v-if="!tab.active && !tab.icon" class="wizard-icon">{{index + 1}}</i>
-            </slot>
-        </transition>
+        <div v-if="tab.active" class="wizard-icon-container"
+             :class="{square_shape:isStepSquare, tab_shape:isTabShape}"
+             :style="[tab.active ? iconActiveStyle: {}, tab.validationError ? errorStyle : {}]">
+          <slot name="active-step">
+            <img v-if="tab.image" :src="tab.image">
+            <i v-else-if="tab.icon" :class="tab.icon" class="wizard-icon"></i>
+            <i v-else class="wizard-icon">{{index + 1}}</i>
+          </slot>
+        </div>
+        <slot v-if="!tab.active">
+          <img v-if="tab.image" :src="tab.image">
+          <i v-else-if="!tab.active && tab.icon" :class="tab.icon" class="wizard-icon"></i>
+          <i v-if="!tab.active && !tab.icon" class="wizard-icon">{{index + 1}}</i>
+        </slot>
 
       </div>
       <slot name="title">
